21b6b97669b2a21f34cc1edae0359bb9adf54f52
Felipe <felipe@visionar.io>
2024-03-30 00:18:38 -0300

    Removed lines

--- project/functions.js
+++ project/functions.js
@@ -13,11 +13,48 @@ function readFolder(folderPath) {
     })
 }
 
-function fileEndsWith(array, def) {
-    return array.filter(el => el.endsWith(def))
+function fileEndsWith(files, def) {
+    return files.filter(el => el.endsWith(def))
+}
+
+function readContentFile(filePath) {
+    return new Promise((resolve, reject) => {
+        try {
+            const content = fs.readFileSync(filePath, { encoding: 'utf-8' })
+            resolve(content.toString())
+        } catch (error) {
+            reject(error)
+        }
+
+    })
+}
+
+function readContentFiles(files) {
+    return Promise.all(
+        files.map(file => readContentFile(file))
+    )
+}
+
+function removeEmptSpace(lines) {
+    return lines.filter(line => line.trim())
+}
+
+function removeIfFound(lines, textDefault) {
+    return lines.filter(line => !line.includes(textDefault))
+}
+
+function removeNumbers(lines) {
+    return lines.filter(line => {
+        const num = parseInt(line.trim())
+        return num !== num
+    })
 }
 
 module.exports = {
     readFolder,
-    fileEndsWith
+    fileEndsWith,
+    readContentFiles,
+    removeEmptSpace,
+    removeIfFound,
+    removeNumbers
 }
