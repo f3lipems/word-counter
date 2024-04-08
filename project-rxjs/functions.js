7d9a94d1e357608690d9b075a7f9edcc6d1653e6
Felipe <felipe@visionar.io>
2024-04-08 10:41:03 -0300

    Observable read file

---
+++ project-rxjs/functions.js
@@ -0,0 +1,110 @@
+const fs = require('fs')
+const path = require('path')
+const { Observable } = require('rxjs')
+
+function readFolder(folderPath) {
+    return new Observable(subscriber => {
+        try {
+            fs.readdirSync(folderPath).forEach(file => {
+                subscriber.next(path.join(folderPath, file))
+            })
+            subscriber.complete()
+        } catch (error) {
+            subscriber.error(error)
+        }
+    })
+}
+
+function fileEndsWith(def) {
+    return function (files) {
+        return files.filter(el => el.endsWith(def))
+    }
+}
+
+function readContentFile(filePath) {
+    return new Promise((resolve, reject) => {
+        try {
+            const content = fs.readFileSync(filePath, { encoding: 'utf-8' })
+            resolve(content.toString())
+        } catch (error) {
+            reject(error)
+        }
+
+    })
+}
+
+function readContentFiles(files) {
+    return Promise.all(
+        files.map(file => readContentFile(file))
+    )
+}
+
+function removeEmptSpace(lines) {
+    return lines.filter(line => line.trim())
+}
+
+function removeIfFound(textDefault) {
+    return function (lines) {
+        return lines.filter(line => !line.includes(textDefault))
+    }
+}
+
+function removeNumbers(lines) {
+    return lines.filter(line => {
+        const num = parseInt(line.trim())
+        return num !== num
+    })
+}
+
+function removeSymbols(symbols) {
+    return function (elements) {
+        return elements.map(el => {
+            return symbols.reduce((acc, symbol) => {
+                return acc.split(symbol).join('')
+            }, el)
+        })
+    }
+}
+
+function joinContent(contents) {
+    return contents.join(' ')
+}
+
+function splitText(symbol) {
+    return function (allContent) {
+        return allContent.split(symbol)
+    }
+}
+
+function groupWords(words) {
+    return Object.values(
+        words.reduce((acc, word) => {
+            const w = word.toLowerCase()
+            const qtt = acc[word] ? acc[word].qtt + 1 : 1
+            acc[word] = { word: w, qtt }
+            return acc
+        }, {})
+    )
+}
+
+function sortByAttr(attr, order = 'asc') {
+    return function (list) {
+        const asc = (el1, el2) => el1[attr] - el2[attr]
+        const desc = (el1, el2) => el2[attr] - el1[attr]
+        return list.sort(order === 'asc' ? asc : desc)
+    }
+}
+
+module.exports = {
+    readFolder,
+    fileEndsWith,
+    readContentFiles,
+    removeEmptSpace,
+    removeIfFound,
+    removeNumbers,
+    removeSymbols,
+    joinContent,
+    splitText,
+    groupWords,
+    sortByAttr
+}
---
+++ project-rxjs/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const fn = require('./functions')
+
+const dataPath = path.join(__dirname, '..', 'data', 'legendas')
+
+const symbols = [
+    '.', '?', '-', ',', '"', '♪', '_', '<i>', '</i>', '\r', '[', ']', '(', ')'
+]
+
+fn.readFolder(dataPath)
+    .subscribe(console.log)
+
+// fn.readFolder(dataPath)
+//     .then(fn.fileEndsWith('.srt'))
+//     .then(fn.readContentFiles)
+//     .then(fn.joinContent)
+//     .then(fn.splitText('\n'))
+//     .then(fn.removeEmptSpace)
+//     .then(fn.removeIfFound('-->'))
+//     .then(fn.removeNumbers)
+//     .then(fn.removeSymbols(symbols))
+//     .then(fn.joinContent)
+//     .then(fn.splitText(' '))
+//     .then(fn.removeEmptSpace)
+//     .then(fn.removeNumbers)
+//     .then(fn.groupWords)
+//     .then(fn.sortByAttr('qtt', 'desc'))
+//     .then(console.log)
